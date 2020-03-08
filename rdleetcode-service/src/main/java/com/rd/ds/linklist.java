4a5dcab1160a9a6b3f0327545ba787cc2611c063
rendong.rd <rendong.rd@alibaba-inc.com>
2020-03-08 18:43:49 +0800

    pitong

---
+++ rdleetcode-service/src/main/java/com/rd/ds/linklist.java
@@ -0,0 +1,14 @@
+/**
+ * Alibaba.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved.
+ */
+
+package com.rd.ds;
+
+/**
+ * @author 邳彤
+ * @version : linklist, v 0.1 2020-03-08 邳彤  Exp $
+ * @Description todo
+ */
+public class linklist {
+}
